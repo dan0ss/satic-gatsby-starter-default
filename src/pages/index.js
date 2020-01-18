666e131a33f6418a6fbfdf01705a75171612c1be
Dan <d_emery@outlook.com.au>
2020-01-18 23:13:19 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,6 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    
-    <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js">
-<!--<![endif]-->
-
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">

48c6509664ca9f866f927b7f26d4a854cb2b538f
Dan <d_emery@outlook.com.au>
2020-01-19 03:03:30 +0800

    fwef

--- src/components/image.js
+++ src/components/image.js
@@ -25,21 +25,8 @@ const Image = () => {
       }
     }
   `)
-  const Ipl = () => {
-    const data = useStaticQuery(graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `)
 
   return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
 }
 
 export default Image
-export default Ipl

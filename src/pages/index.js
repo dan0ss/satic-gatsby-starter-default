4eb98093e54bb25026ae7fdd135bcb26117e9cad
Dan <d_emery@outlook.com.au>
2020-01-19 15:27:34 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -465,7 +465,7 @@ const IndexPage = () => (
     </section>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Img />
-      <img fixed={file.testImg.fixed} />
+      <Img fixed={data.file.testImg.fixed} />
     </div>
 
 

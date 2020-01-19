6a97fc1d40758aad54603ece4bb995be3fe9c104
Dan <d_emery@outlook.com.au>
2020-01-19 15:52:27 +0800

    images from github

--- src/pages/index.js
+++ src/pages/index.js
@@ -466,6 +466,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Img />
       <Img fixed={testImg} />
+      <Img fluid={props.data.imageOne.childImageSharp.fluid} />
+      <Img fluid={props.data.imageTwo.childImageSharp.fluid} />
+      <Img fluid={props.data.imageThree.childImageSharp.fluid} />
     </div>
 
 
@@ -474,3 +477,27 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const fluidImage = graphql`
+fragment fluidImage on File {
+  childImageSharp {
+    fluid(maxWidth: 1000) {
+      ...GatsbyImageSharpFluid
+    }
+  }
+}
+`;
+
+export const pageQuery = graphql`
+  query {
+    imageOne: file(relativePath: { eq: "one.jpg" }) {
+      ...fluidImage
+    }
+    imageTwo: file(relativePath: { eq: "two.jpg" }) {
+      ...fluidImage
+    }
+    imageThree: file(relativePath: { eq: "three.jpg" }) {
+      ...fluidImage
+    }
+  }
+  `

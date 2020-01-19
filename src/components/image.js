33b8ef380eb02db28973fd6041eeb0c83ed42f2a
Dan <d_emery@outlook.com.au>
2020-01-20 04:40:19 +0800

    wefwef

--- src/components/image.js
+++ src/components/image.js
@@ -26,22 +26,22 @@ fragment fluidImage on File {
 
 export const pageQuery = graphql`
   query {
-    logoWhite: file(relativePath: { eq: "/images/perth-ipl-centre.svg" }) {
+    logoWhite: file(relativePath: { eq: "images/perth-ipl-centre.svg" }) {
       ...fluidImage
     }
-    iplHero: file(relativePath: { eq: "/images/layoutipl-hero.jpg" }) {
+    iplHero: file(relativePath: { eq: "images/layoutipl-hero.jpg" }) {
       ...fluidImage
     }
-    faceIpl: file(relativePath: { eq: "/images/face.jpg" }) {
+    faceIpl: file(relativePath: { eq: "images/face.jpg" }) {
       ...fluidImage
     }
-    mensIpl: file(relativePath: { eq: "/images/hair-removal.jpeg" }) {
+    mensIpl: file(relativePath: { eq: "images/hair-removal.jpeg" }) {
       ...fluidImage
     }
-    legsIpl: file(relativePath: { eq: "/images/leg.jpg" }) {
+    legsIpl: file(relativePath: { eq: "images/leg.jpg" }) {
       ...fluidImage
     }
-    pigmentationIpl: file(relativePath: { eq: "/images/pigmentation.jpeg" }) {
+    pigmentationIpl: file(relativePath: { eq: "images/pigmentation.jpeg" }) {
       ...fluidImage
     }
   }

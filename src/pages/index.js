c5056cb78ee13f0409878f7e43da03f0690fe51c
Dan <d_emery@outlook.com.au>
2020-01-19 16:16:10 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Img from 'gatsby-image'
+import Img from 'components/image'
 import testImg from "../images/face.jpg"
 import SEO from "../components/seo"
 

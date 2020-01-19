c2c08071c6d4cd4ee6d067a8e07ea32138a1467d
Dan <d_emery@outlook.com.au>
2020-01-19 19:19:02 +0800

    Update image.js

--- src/components/image.js
+++ src/components/image.js
@@ -14,5 +14,29 @@ import Img from "gatsby-image"
  */
 
 
+export const fluidImage = graphql`
+fragment fluidImage on File {
+  childImageSharp {
+    fluid(maxWidth: 1000) {
+      ...GatsbyImageSharpFluid
+    }
+  }
+}
+`;
+
+export const pageQuery = graphql`
+  query {
+    imageOne: file(relativePath: { eq: "one.jpg" }) {
+      ...fluidImage
+    }
+    imageTwo: file(relativePath: { eq: "two.jpg" }) {
+      ...fluidImage
+    }
+    imageThree: file(relativePath: { eq: "three.jpg" }) {
+      ...fluidImage
+    }
+  }
+`
+
 
 export default Image

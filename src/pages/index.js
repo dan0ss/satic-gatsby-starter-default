9aa0bd5645165e55595c905d1c62350c2b4a3f99
Dan <d_emery@outlook.com.au>
2020-01-20 01:27:01 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,26 +18,3 @@ const IndexPage = (props) => (
 
 export default IndexPage
 
-export const fluidImage = graphql`
-fragment fluidImage on File {
-  childImageSharp {
-    fluid(maxWidth: 1000) {
-      ...GatsbyImageSharpFluid
-    }
-  }
-}
-`;
-
-export const pageQuery = graphql`
-  query {
-    imageOne: file(relativePath: { eq: "one.jpg" }) {
-      ...fluidImage
-    }
-    imageTwo: file(relativePath: { eq: "two.jpg" }) {
-      ...fluidImage
-    }
-    imageThree: file(relativePath: { eq: "three.jpg" }) {
-      ...fluidImage
-    }
-  }
-`

3de540cc67f857e6a635816e4fba5ac7ea019f9b
Dan <d_emery@outlook.com.au>
2020-01-20 22:02:53 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-import Image from '../components/images'
+import Image from 'gatsby-image'
 
 
 import Layout from '../components/layout'
